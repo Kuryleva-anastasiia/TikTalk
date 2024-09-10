6c70857092b8c1af1b09b5966efe283a196e5667
Kuryleva Anastasia <knastya76@yandex.ru>
2024-09-10 15:14:27 +0300

    input для ввода текста поста

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
+import { AvatarCircleComponent } from "../../../common-ui/avatar-circle/avatar-circle.component";
+import { ProfileService } from '../../../data/services/profile.service';
+import { ImgUrlPipe } from '../../../helpers/pipes/img-url.pipe';
+import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-post-input',
   standalone: true,
-  imports: [],
+  imports: [AvatarCircleComponent, SvgIconComponent],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
+  profile = inject(ProfileService).me
+  r2 = inject(Renderer2)
+  
+  onTextAreaInput(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement
 
+    this.r2.setStyle(textarea, 'height', 'auto');
+    this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
 }
